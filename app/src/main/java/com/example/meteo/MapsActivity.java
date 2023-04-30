f4ddbba2426f6c3123f0cc923fd0debb23ca51a8
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-04-30 16:43:52 +0200

    new

--- app/src/main/java/com/example/meteo/MapsActivity.java
+++ app/src/main/java/com/example/meteo/MapsActivity.java
@@ -1,89 +1,43 @@
 package com.example.meteo;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
+import androidx.fragment.app.FragmentActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-    Location currentLocation;
-
-    FusedLocationProviderClient fusedLocationProviderClient;
-    private static final int REQUEST_CODE = 101;
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLocation();
-    }
-
-    private void fetchLocation() {
-
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_CODE);
-            return;
-        }
-
-        LocationManager locationManager = (LocationManager)  this.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
-
-        Location location = locationManager.getLastKnownLocation(bestProvider);
-        currentLocation = location;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Intent intent = getIntent();
-        LatLng latLng = new LatLng(Double.parseDouble(intent.getStringExtra("lat")), Double.parseDouble(intent.getStringExtra("lon")));
-
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I'm here !");
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 2));
-        googleMap.addMarker(markerOptions);
-
-
+        mMap = googleMap;
+        LatLng twincenter = new LatLng(33.586685, -7.632254);
+        mMap.addMarker(new MarkerOptions().position(twincenter).title("Twin Center"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(twincenter));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(twincenter,10));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(twincenter);
+        circleOptions.radius(700);
+        circleOptions.fillColor(Color.TRANSPARENT);
+        circleOptions.strokeWidth(6);
+        mMap.addCircle(circleOptions);
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    fetchLocation();
-                }
-                break;
-        }
-    }
-
 }
